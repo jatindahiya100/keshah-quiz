15eb9b1c95e639f67abba7fdd755768b7ef3e7f4
Jatin Dahiya <jatindahiya100@gmail.com>
2024-10-19 14:50:58 +0530

    Updated 19-10-2024

--- src/components/QuizCompleted.jsx
+++ src/components/QuizCompleted.jsx
@@ -3,9 +3,33 @@ import { motion } from 'framer-motion';
 import StripeCheckoutForm from './StripeCheckoutForm';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
+import finalScreenPhotoGrid01 from '../assets/media/Daily Routine/finalScreenPhotoGrid01.png';
+import finalScreenPhotoGrid02 from '../assets/media/Daily Routine/finalScreenPhotoGrid02.png';
+import finalScreenPhotoGrid03 from '../assets/media/Daily Routine/finalScreenPhotoGrid03.png';
+import finalScreenPhotoGrid04 from '../assets/media/Daily Routine/finalScreenPhotoGrid04.png';
+import finalScreenPhotoGrid05 from '../assets/media/Daily Routine/finalScreenPhotoGrid05.png';
+import finalScreenPhotoGrid06 from '../assets/media/Daily Routine/finalScreenPhotoGrid06.png';
+import finalScreenPhotoGrid07 from '../assets/media/Daily Routine/finalScreenPhotoGrid07.png';
+import finalScreenPhotoGrid08 from '../assets/media/Daily Routine/finalScreenPhotoGrid08.png';
+import finalScreenPhotoGrid09 from '../assets/media/Daily Routine/finalScreenPhotoGrid09.png';
+import finalScreenPhotoGrid10 from '../assets/media/Daily Routine/finalScreenPhotoGrid10.png';
+
 
 // Load Stripe with your publishable key
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
+const images = [
+    finalScreenPhotoGrid01,
+    finalScreenPhotoGrid02,
+    finalScreenPhotoGrid03,
+    finalScreenPhotoGrid04,
+    finalScreenPhotoGrid05,
+    finalScreenPhotoGrid06,
+    finalScreenPhotoGrid07,
+    finalScreenPhotoGrid08,
+    finalScreenPhotoGrid09,
+    finalScreenPhotoGrid10,
+];
 
 export default function QuizCompleted() {
     return (
@@ -44,13 +68,45 @@ export default function QuizCompleted() {
                 </b>
                 <p className='text-white text-3xl leading-loose tracking-wide bg-transparent text-center'>13:22</p>
             </div>
-
+            <p className='text-white font-light text-2xl'>Join thousands of men regrowing their hair</p>
+            <div className='flex gap-8'>
+                <div className="px-6 py-6 border rounded-2xl flex flex-col gap-1 justify-center">
+                    <span className='text-[#369033] text-xl'>85%</span>
+                    <p className='text-white text-sm font-extralight'>See regrowth post treatment.</p>
+                </div>
+                <div className="px-6 py-6 border rounded-2xl flex flex-col gap-1 justify-center">
+                    <span className='text-[#369033] text-xl'>0%</span>
+                    <p className='text-white text-sm font-extralight'>Reported side-effect risk.</p>
+                </div>
+            </div>
+            <div className='grid grid-cols-5'> {/* Optional: add gap for spacing */}
+                {images.map((image, index) => (
+                    <img key={index} src={image} alt={`Photo ${index + 1}`} className='object-cover w-full h-full' />
+                ))}
+            </div>
+            <div className='px-6 py-6 border rounded-2xl flex flex-col gap-6 justify-center'>
+                <div className='flex items-center justify-between'>
+                    <p className='text-white text-sm font-extralight'>Total Today</p>
+                    <p className='text-white text-sm font-extralight'>$29.99 <strike>$99.99</strike> <span className='bg-[#369033] p-1 rounded text-xs font-normal'>Saving $70</span> </p>
+                </div>
+                <div className='flex items-center justify-between'>
+                    <p className='text-white text-sm font-extralight'>Price after first month</p>
+                    <p className='text-white text-sm font-extralight'>$99.99/month</p>
+                </div>
+                <p className='text-white text-sm font-extralight text-center'>No commitment. Cancel anytime. </p>
+            </div>
             <div className='bg-[#242424] rounded-xl p-6 mt-4'>
                 <h2 className='text-white text-2xl font-light mb-4 bg-transparent'>Billing Details</h2>
                 <p></p>
                 <Elements stripe={stripePromise}>
                     <StripeCheckoutForm />
                 </Elements>
+            </div>
+            <div className='bg-[#242424] rounded-xl flex flex-col items-center gap-1 px-12 py-6'>
+                <p className='text-white font-light text-sm leading-loose tracking-wide bg-transparent text-center'>
+                    100% Money Back Guarantee.
+                </p>
+                <p className='text-white font-light text-sm leading-loose tracking-wide bg-transparent text-justify'>Enjoy extra peace of mind. You can claim a full refund within 30 days if you’ve completed at least 80% of the sessions and still feel KESHAH isn’t right for you. </p>
             </div>
         </div>
     );
