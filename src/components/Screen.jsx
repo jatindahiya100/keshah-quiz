879d2e20773599569dcb02b2528a706bedbac4ab
Jatin Dahiya <jatindahiya100@gmail.com>
2024-10-21 13:05:46 +0530

    Updated 21-10-2024

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -178,7 +178,7 @@ const Screen = ({ data, onNext, onPrevious, onInputChange, answers, progress }) 
                     <path d="M6.1584 3.13508C6.35985 2.94621 6.67627 2.95642 6.86514 3.15788L10.6151 7.15788C10.7954 7.3502 10.7954 7.64949 10.6151 7.84182L6.86514 11.8418C6.67627 12.0433 6.35985 12.0535 6.1584 11.8646C5.95694 11.6757 5.94673 11.3593 6.1356 11.1579L9.565 7.49985L6.1356 3.84182C5.94673 3.64036 5.95694 3.32394 6.1584 3.13508Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" />
                 </svg>
             </motion.button>
-            <div className='w-full'>
+            <div className='mt-8 w-full'>
                 <div className='w-full h-[0.25px] bg-gray-600'></div>
                 <ul className='pt-8 w-full inline-flex items-center gap-8 text-gray-400 text-xs font-extralight'>
                     <li className='cursor-pointer hover:text-white transition-all ease-in-out duration-300'>Home</li>
